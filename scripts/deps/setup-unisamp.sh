8ed7174fc3d1d3dc396d27250a4f916718a6b5a6
Arijit Shaw <if.arijit@gmail.com>
2023-12-05 16:43:55 +0530

    Update setup-unisamp to put library links in bashrc

--- scripts/deps/setup-unisamp.sh
+++ scripts/deps/setup-unisamp.sh
@@ -4,6 +4,8 @@ set -e -u -o pipefail
 
 dep_dir="deps"
 install_dir=$(readlink -fm "${dep_dir}"/install)
+lib_dir=$(readlink -fm "${dep_dir}"/install/lib/)
+echo "export LD_LIBRARY_PATH=$lib_dir:\$LD_LIBRARY_PATH" >> ~/.bashrc
 
 [ ! -d "${install_dir}" ] && mkdir -p "${install_dir}"
 
